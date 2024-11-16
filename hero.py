5af5c8e019cdd12ae2f4b9ef9da2bf0682c6fa5b
xchbx <ceina.ellison@gmail.com>
2024-11-16 16:01:23 -0500

    build out the hero class

---
+++ hero.py
@@ -0,0 +1,24 @@
+class Hero:
+    # We want our hero to have a default "starting_health",
+    # so we can set that in the function header.
+    def __init__(self, name, starting_health=100):
+        '''Instance properties:
+        name: String
+        starting_health: Integer
+        current_health: Integer
+        '''
+        # We know the name of our hero, so we assign it here
+        self.name = name
+        # similarly, our starting health is passed in, just like name
+        self.starting_health = starting_health
+        # when a hero is created, their current health is
+        # always the same as their starting health (no damage taken yet!)
+        self.current_health = starting_health
+
+
+if __name__ == "__main__":
+    # If you run this file from the terminal
+    # this block is executed.
+    my_hero = Hero("Grace Hopper", 200)
+    print(my_hero.name)
+    print(my_hero.current_health)
