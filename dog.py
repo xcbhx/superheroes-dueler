f3ea6a773bc9e6b7b031cef8cf75c9f0095c584d
xchbx <ceina.ellison@gmail.com>
2024-11-16 15:26:53 -0500

    created dog class

--- dog.py
+++ dog.py
@@ -1,4 +1,18 @@
 class Dog:
-    def __init__(self, name):
+    def __init__(self, name, breed):
         self.name = name
+        self.breed = breed
         print("dog initialized!")
+
+
+    # Methods are defined as their own named functions inside the class
+    def bark(self):
+        print("Woof!")
+
+    
+# message = "Hello World!"
+
+# def greeting():
+#     print(message)
+
+# greeting()
---
+++ my_dogs.py
@@ -0,0 +1,9 @@
+import dog
+
+
+# instantiation call that creates a Dog object:
+my_dog = dog.Dog("Rex", "SuperDog")
+my_dog.bark()
+
+my_other_dog = dog.Dog("Annie", "SuperDog")
+print(my_other_dog.name)
