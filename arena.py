dad639e58a8b764d485fc41e9aa62abcd5d8d781
xchbx <ceina.ellison@gmail.com>
2024-11-18 18:57:58 -0500

    created class Arena

---
+++ arena.py
@@ -0,0 +1,14 @@
+from ability import Ability
+from weapon import Weapon
+from armor import Armor
+from hero import Hero
+from team import Team 
+
+class Arena:
+    def __init__(self):
+        '''Instantiate properties
+        team_one: None
+        team_two: None
+        '''
+        self.team_one = None
+        self.team_two = None
