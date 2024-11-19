fe4669d9a6ca90d2f94f3ee955ce08310246d747
xchbx <ceina.ellison@gmail.com>
2024-11-18 20:31:22 -0500

    added create_weapon method

--- arena.py
+++ arena.py
@@ -20,4 +20,21 @@ class Arena:
         name = input('What is the ability name?')
         max_damage = input('What is the max damage of the ability?')
 
-        return Ability(name, max_damage)
+        return Ability(name, max_damage) 
+    
+    def create_weapon(self):
+        '''Prompt user for Weapon information
+        return Weapon with values from user input. 
+        '''
+        # TODO: This method will allow a user to create a weapon.
+        # Prompt the user for the necessary information to create a new weapon object.
+        weapon_name = input('Enter the name of weapon: ')
+        # Prompt the user for the weapon's attack damage
+        try:
+            weapon_damage = int(input(f'Enter the attack damage for {weapon_name}: '))
+        except ValueError:
+            print("Invalid input. Attack damage must be a number. Defaulting to 10.")
+        weapon_damage = 10 
+
+        # return the new weapon object.
+        return Weapon(weapon_name, weapon_damage)
